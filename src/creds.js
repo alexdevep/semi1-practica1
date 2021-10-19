f7aaf79b8f21a8e0d9866db016e97b9635f2d370
alexdevep <69699042+alexdevep@users.noreply.github.com>
2021-10-19 17:57:02 -0600

    Update creds.js

--- src/creds.js
+++ src/creds.js
@@ -1,25 +1,25 @@
 let aws_keys = {
     s3: {
         region: 'us-east-2',
-        accessKeyId: "AKIAUFAWXK2WZQZGC3KH",
-        secretAccessKey: "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",
+        accessKeyId: "accessKeyId",
+        secretAccessKey: "secretAccessKey",
         //apiVersion: '2006-03-01',
     },
     dynamodb: {
         apiVersion: '2012-08-10',
         region: 'us-east-2',
-        accessKeyId: "AKIAUFAWXK2W6B3NR254",
-        secretAccessKey: "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"
+        accessKeyId: "accessKeyId",
+        secretAccessKey: "secretAccessKey"
     },
     rekognition: {
         region: 'us-east-2',
-        accessKeyId: "AKIAUFAWXK2WWLCNXOP2",
-        secretAccessKey: "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" 
+        accessKeyId: "accessKeyId",
+        secretAccessKey: "secretAccessKey" 
     },
     translate: {
         region: 'us-east-2',
-        accessKeyId: "AKIAUFAWXK2WZCJ7ZS6R",
-        secretAccessKey: "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" 
+        accessKeyId: "accessKeyId",
+        secretAccessKey: "secretAccessKey" 
     }
 }
 module.exports = aws_keys
