cc7ff789284b0fc4214428aaa9ddc0056df9a6d7
alexdevep <69699042+alexdevep@users.noreply.github.com>
2021-10-19 17:58:03 -0600

    Update db_creds.js

--- src/models/db_creds.js
+++ src/models/db_creds.js
@@ -1,8 +1,8 @@
 let db_credentials = {
-    host: 'mysqldb.cahzfivbuo9y.us-east-2.rds.amazonaws.com',
+    host: 'mysqldb.rds.amazonaws.com',
     port: 3306,
     user: 'admin',
-    password:'admin_mysql',
-    database: 'practica1'
+    password:'password',
+    database: 'nombre_bd'
 }
 module.exports = db_credentials;
